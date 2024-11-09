40377c3f9fd0c8be34d2936db444f79cee422b74
nikhilnv1209 <nikhilverma5000.nv@gmail.com>
2024-11-09 14:56:42 +0530

    Feat(add and list methods): Added the list and add methods to add a new todo in the list and also in the sqlite database Added the function to have the list of all the todos from the database.

--- cmd/add.go
+++ cmd/add.go
@@ -1,8 +1,50 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"time"
+	"todos-app/internal/db"
+
+	"github.com/google/uuid"
+	"github.com/spf13/cobra"
+)
+
+var title, status string
 
 var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "This will add the todos in the main todo list",
+	Run: func(cmd *cobra.Command, args []string) {
+		if title == "" {
+			fmt.Println("please specify the title for the todos")
+			return
+		}
+
+		status, err := addTodos(title, status)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(status)
+		}
+	},
+}
+
+func addTodos(title string, status string) (string, error) {
+	id := uuid.NewString()
+	createdAt := time.Now().Format("2006-01-02 15:04:05")
+	updatedAt := createdAt
+
+	insertQuery := `INSERT INTO todos (id, title, status, createdAt, updatedAt) VALUES (?,?,?,?,?)`
+
+	_, err := db.Client.Exec(insertQuery, id, title, status, createdAt, updatedAt)
+	if err != nil {
+		return "", fmt.Errorf("error adding todo %v", err)
+	}
+
+	return "todo added succesfully", nil
+}
+
+func init() {
+	addCmd.Flags().StringVarP(&title, "title", "t", "", "Title of the todo")
+	addCmd.Flags().StringVarP(&status, "status", "s", "Pending", "Status for the todos")
 }
--- cmd/list.go
+++ cmd/list.go
@@ -1,8 +1,53 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"todos-app/internal/db"
+	"todos-app/internal/types"
+
+	"github.com/spf13/cobra"
+)
 
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List down all the todos in the form of a table",
+	Run: func(cmd *cobra.Command, args []string) {
+		List()
+	},
+}
+
+func List() {
+	listQuery := `SELECT * FROM todos`
+
+	result, err := db.Client.Query(listQuery)
+
+	if err != nil {
+		fmt.Printf("error while listing the todos %v", err)
+		return
+	}
+
+	defer result.Close()
+
+	hasRows := false
+
+	for result.Next() {
+		hasRows = true
+		var todo types.Todo
+		err := result.Scan(&todo.Id, &todo.Title, &todo.Status, &todo.CreatedAt, &todo.UpdatedAt)
+
+		if err != nil {
+			fmt.Println("error while scanning the row")
+			continue
+		}
+
+		fmt.Println(todo.Id, todo.Title, todo.Status, todo.CreatedAt, todo.UpdatedAt)
+	}
+
+	if !hasRows {
+		fmt.Println("No rows found")
+	}
+
+	if err = result.Err(); err != nil {
+		fmt.Println("Error reading rows", err)
+	}
 }
